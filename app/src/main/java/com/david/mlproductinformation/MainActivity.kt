45673f23ee55462d5ac385fe5baff608ebd11b0e
David Guedez <david.guedez@frogmi.com>
2023-04-23 16:28:09 -0400

    Added Navigation and Search package with initial fragment

--- app/src/main/java/com/david/mlproductinformation/MainActivity.kt
+++ app/src/main/java/com/david/mlproductinformation/MainActivity.kt
@@ -2,10 +2,21 @@ package com.david.mlproductinformation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.david.mlproductinformation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/david/mlproductinformation/search/presentation/SearchProductViewModel.kt
@@ -0,0 +1,6 @@
+package com.david.mlproductinformation.search.presentation
+
+import androidx.lifecycle.ViewModel
+
+class SearchProductViewModel: ViewModel() {
+}
