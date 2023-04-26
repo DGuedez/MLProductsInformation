9a20ad1cb79e8b840013f4fe852bfa5063d415c5
David Guedez <david.guedez@frogmi.com>
2023-04-26 01:42:13 -0400

    Implement app styles colors

---
+++ app/src/test/java/com/david/mlproductinformation/products/presentation/ProductsFragmentTest.kt
@@ -0,0 +1,17 @@
+package com.david.mlproductinformation.products.presentation
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class ProductsFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
